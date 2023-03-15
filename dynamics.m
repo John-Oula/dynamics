cb071e6424a03ff38a30566ecd31379d53a6af91
John-Oula <johnoula@antratechstudios.com>
2023-03-15 22:59:50 +0800

    Create dynamics.m

---
+++ dynamics.m
@@ -0,0 +1,18 @@
+% Flight Conditions
+rho = 0.002377;  
+U0 = 165; 
+theta_0 = 0;
+m = 564000; 
+g = 9.81;
+
+% Moments of Inertia about x,y,z axis respectively
+
+Ixx= 12.52;
+Iyy = 8.43;
+Izz= 18.55;
+Ixz = 0; % assumed
+
+% UAV geometry data
+S = 5500;  
+b = 195.68;  
+c = 27.31; 
