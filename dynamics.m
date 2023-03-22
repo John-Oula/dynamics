da25507c653ba8bb53c309345e190243c30d3ff4
John-Oula <johnoula@antratechstudios.com>
2023-03-22 18:20:54 +0800

    step response of CL system

--- dynamics.m
+++ dynamics.m
@@ -1,18 +1,39 @@
+
+
+clc
+close all
+clear
+
 % Flight Conditions
-rho = 0.002377;  
-U0 = 165; 
-theta_0 = 0;
-m = 564000; 
-g = 9.81;
+theta_0 = 0;    
+m = 564000;      % in lb
+h = 80000;        % Altitude
+rho = 0.002377; % slugs/cubic feet
+rho = 0.0765; % lb/ft^3
+U_0 = 165; % Knots
+U_0 = U_0 * 1.68781; % to feet/second
+
+% Constants
+
+a = 340; %  velocity in m/s
+g = 9.80665; % acceleration due to gravity
+
 
 % Moments of Inertia about x,y,z axis respectively
 
-Ixx= 12.52;
-Iyy = 8.43;
-Izz= 18.55;
-Ixz = 0; % assumed
+Iyy = 32.3e6; % slug-ft^2
+Ixx = 14.3e6;
+Izz = 45.3e6;
+Ixz = -2.23e6;
 
 % UAV geometry data
-S = 5500;  
-b = 195.68;  
-c = 27.31; 
+
+S = 5500; % square feet
+b = 195.68; % feet
+c = 27.31; % feet
+
+% engine data
+Pe = [17.63/12; 0;-14.92/12];
+To = [9.6757 ;0;0];
+
+save("dynamics.mat")
---
+++ nonDimDerivatives.m
@@ -0,0 +1,21 @@
+% Non-Dimensional Stability derivates
+
+% Longitudinal Non-Dimensional Aerodynamic derivatives
+% da namely, alpha dot
+% de namely, delta "e"
+
+C_L = 1.11;
+C_D = 0.102;
+C_La = 5.7;
+C_D_a = 0.66;
+C_m_a = -1.26;
+C_L_da = -6.7;
+C_m_da = -3.2;
+C_L_hq = 5.4;
+C_m_hq = -20.8;
+C_L_M = -0.81;
+C_m_M = 0.27;
+C_L_de = 0.388;
+C_m_de = -1.34;
+
+save('nonDimDerivatives.mat');
